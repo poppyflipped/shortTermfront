af8c6f8ece21cb2ef525fadd48c0675e5e27ddc6
sjy <2372586549@qq.com>
2025-07-02 16:59:40 +0800

    检查

--- config/index.js
+++ config/index.js
@@ -1,11 +1,6 @@
-/*
- * @Description: 
- * @Author: Rabbiter
- * @Date: 2023-03-03 20:03:53
- */
+
 'use strict'
-// Template version: 1.3.1
-// see http://vuejs-templates.github.io/webpack for documentation.
+
 
 const path = require('path')
 
@@ -74,10 +69,7 @@ module.exports = {
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
 
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
+ 
     bundleAnalyzerReport: process.env.npm_config_report
   }
 }
